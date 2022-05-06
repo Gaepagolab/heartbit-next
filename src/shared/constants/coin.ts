79ae8255628aac0f5069bd6584304ee5c71b6e6f
Yongjoo Choi <yongjoo.choi@nearthlab.com>
2022-05-06 14:02:04 +0900

    add coin constants

--- src/shared/constants/coin.ts
+++ src/shared/constants/coin.ts
@@ -11,3 +11,10 @@ export const CoinUSDT = {
   [CoinType.XRP]: "xrpusdt",
   [CoinType.EOS]: "eosusdt",
 };
+
+export const CoinKR = {
+  [CoinType.BTC]: "비트 코인",
+  [CoinType.ETH]: "이더리움",
+  [CoinType.XRP]: "리플",
+  [CoinType.EOS]: "이오스",
+};
